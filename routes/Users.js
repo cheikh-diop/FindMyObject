c21197dedb1cb9cc67178f009835378490e1e60d
Omar <omar.nour33@gmail.com>
2017-11-30 18:24:56 +0100

    ajout utilisateur

--- routes/Users.js
+++ routes/Users.js
@@ -2,3 +2,11 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var login = require('../models/Users.js');
+
+/* SAVE USER */
+router.post('/', function(req, res, next) {
+    Users.create(req.body, function (err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });
+  });
