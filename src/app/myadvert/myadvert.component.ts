633bf7c51cfd0d0c3014d3e41cc2ca563b6698c0
cheikh-diop <cdiop288@gmail.com>
2017-12-09 16:16:06 +0100

    Ajout des fonctionalite ajout annonce et Gestion de mes annonces pour un utilisateur qui a perdu un objet

--- src/app/myadvert/myadvert.component.ts
+++ src/app/myadvert/myadvert.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { LoginComponent } from '../login/login.component'
+import { Http, RequestOptions, Headers, URLSearchParams, ResponseContentType, Response } from '@angular/http';
+import { Body } from '@angular/http/src/body';
 @Component({
   selector: 'app-myadvert',
   templateUrl: './myadvert.component.html',
   styleUrls: ['./myadvert.component.css']
 })
 export class MyadvertComponent implements OnInit {
-
-  constructor() { }
+  advert: any;
+  user = { 
+    _id :""
+  }
+  constructor(private router: Router, private http: Http, private auth: LoginComponent) { }
 
   ngOnInit() {
+    
+    this.auth.getProfile().subscribe(profile => {
+      this.user._id = profile.user._id;
+
+        console.log(this.user._id)
+      this.http.get('http://localhost:3000/user/getUserAdvert/'+this.user._id).subscribe(data => {
+      this.advert = data.json().advert;
+    });
+    },
+      err => {
+        console.log(err);
+        return false;
+      }
+    )
+    
+
+    
   }
 
 }
