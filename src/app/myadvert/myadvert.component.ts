7318db792638be60f1c2fa8912f8472d3645e1f3
cheikh-diop <cdiop288@gmail.com>
2017-12-16 16:43:39 +0100

    maj

--- src/app/myadvert/myadvert.component.ts
+++ src/app/myadvert/myadvert.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { LoginComponent } from '../login/login.component'
+import {MatchingService} from '../matching.service'
+import { Http, RequestOptions, Headers, URLSearchParams, ResponseContentType, Response } from '@angular/http';
+import { Body } from '@angular/http/src/body';
 @Component({
   selector: 'app-myadvert',
   templateUrl: './myadvert.component.html',
   styleUrls: ['./myadvert.component.css']
 })
 export class MyadvertComponent implements OnInit {
-
-  constructor() { }
+  advert: any;
+  user = {
+    _id: "",
+    idadvert:""
+  }
+  constructor(private matchService : MatchingService,private router: Router, private http: Http, private auth: LoginComponent) { }
 
   ngOnInit() {
+
+    this.auth.getProfile().subscribe(profile => {
+      this.user._id = profile.user._id;
+
+      console.log(this.user._id)
+      this.http.get('http://localhost:3000/user/getUserAdvert/' + this.user._id).subscribe(data => {
+        this.advert = data.json().advert;
+      });
+    },
+      err => {
+        console.log(err);
+        return false;
+      }
+    )
   }
+  
+  matchAdvert(advert){
+    //this.matchService.matchAdvert(advert).subscribe
+
+
+    
+  }
+
+  deleteAdvert(id) {
+   alert("suppression");
+    this.user.idadvert=id;
+    this.http.put('http://localhost:3000/user/deleteUserAdvert/',this.user)
+      .subscribe(res => {
+        alert ("suppression faite");
+        console.log(res)
+      }, (err) => {
+        console.log(err);
+      }
+      );
+     // this.router.navigate(['/myadvert']);
+  }
+
+ 
 
 }
